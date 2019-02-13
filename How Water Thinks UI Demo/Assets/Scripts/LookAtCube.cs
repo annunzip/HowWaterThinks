61668b5645583c28391fccffb74453051601b8cc
reesb <10052971+reesb@users.noreply.github.com>
2019-02-13 14:09:53 -0500

    added blue molecules and camera scripts

---
+++ How Water Thinks UI Demo/Assets/Scripts/LookAtCube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCube : MonoBehaviour {
+
+	GameObject membraneCube;
+	Transform membraneTransform;
+
+	// Use this for initialization
+	void Start () {
+
+		membraneCube = GameObject.FindGameObjectWithTag ("Boxes");
+
+		Debug.Log ("Look at = " + membraneCube.name);
+
+		membraneTransform = membraneCube.transform;
+		transform.LookAt (membraneTransform);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.LookAt (membraneTransform);
+		
+	}
+}
