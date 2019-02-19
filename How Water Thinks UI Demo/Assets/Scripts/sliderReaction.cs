5f9e4318b3211df54896d16e7ce71b9c88622354
charleshiggins <charleshiggins2112@gmail.com>
2019-02-19 01:19:47 -0500

    2.19 can remove ions with slider, changed bounds to 0-30

--- How Water Thinks UI Demo/Assets/Scripts/sliderReaction.cs
+++ How Water Thinks UI Demo/Assets/Scripts/sliderReaction.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class sliderReaction : MonoBehaviour {
 
@@ -13,8 +14,8 @@ public class sliderReaction : MonoBehaviour {
         //if (ionSlider == null) ionSlider = GameObject.FindGameObjectWithTag("NumIons").GetComponent<Slider>();
 
         //GameObject atoms = GameObject.FindGameObjectWithTag("Atoms");
-        int numberOfSodiumAtoms = 50;
-        int numberOfChlorineAtoms = 50;
+        int numberOfSodiumAtoms = 15;
+        int numberOfChlorineAtoms = 15;
 
         //Slider ionSlider = GameObject.FindGameObjectWithTag("NumIons").GetComponent<Slider>();
 }
@@ -26,22 +27,19 @@ public class sliderReaction : MonoBehaviour {
         int numAtoms = GameObject.FindGameObjectsWithTag("SodiumAtom").Length + GameObject.FindGameObjectsWithTag("ChlorineAtom").Length;
 
         Debug.Log("numAtoms = " + numAtoms);
-        Debug.Log("ionSlider.value = " + ionSlider.value);
-
-        if (numAtoms/2 < ionSlider.value)
-        {
-            while (numAtoms/2 < ionSlider.value)
-            {
-                GameObject.FindGameObjectWithTag("Atoms").GetComponent<CreateAtoms>().AddNAtoms(1, 1, (int)(ionSlider.value - numAtoms/2));
+        Debug.Log("ionSlider.value = " + (int)(ionSlider.value));
+        if (numAtoms/2 < (int)(ionSlider.value)){
+            while (numAtoms/2 < (int)(ionSlider.value)){
+                GameObject.FindGameObjectWithTag("Atoms").GetComponent<CreateAtoms>().AddNAtoms(1, 1, (int)Math.Floor(ionSlider.value - numAtoms/2));
                 numAtoms += 2;
             }
 
-        } else if (numAtoms / 2 > ionSlider.value)
-        {
-            while (numAtoms / 2 > ionSlider.value)
-            {
-                Destroy(GameObject.FindGameObjectsWithTag("SodiumAtom")[0]);
-                Destroy(GameObject.FindGameObjectsWithTag("ChlorineAtom")[0]);
+        } 
+        else if (numAtoms / 2 > (int)(ionSlider.value)){
+            int destroyNum  = (int)(numAtoms / 2)-(int)(ionSlider.value);
+            for(int i=0; i < destroyNum; i++){
+                Destroy(GameObject.FindGameObjectsWithTag("SodiumAtom")[i]);
+                Destroy(GameObject.FindGameObjectsWithTag("ChlorineAtom")[i]);
                 numAtoms -= 2;
             }
         }
