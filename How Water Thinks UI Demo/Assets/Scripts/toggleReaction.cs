e0636bba1a3a4248cbafd0ec01c9817febdb681d
reesb <10052971+reesb@users.noreply.github.com>
2019-02-19 18:24:03 -0500

    2.19 added toggleReaction script and attatched it to toggles, doesn't do anything right now

---
+++ How Water Thinks UI Demo/Assets/Scripts/toggleReaction.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class toggleReaction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void HandleSodiumToggle(Toggle sodiumToggle)
+    {
+        /*if (GameObject.FindGameObjectsWithTag("SodiumToggle").isOn() == true)
+        {
+            GameObject.FindGameObjectWithTag("Atoms").CreateAtoms();
+        } else
+        {
+
+        }*/
+    }
+
+    public void HandleChlorineToggle(Toggle chlorineToggle)
+    {
+
+    }
+
+    public void HandlePotassiumToggle(Toggle potassiumToggle)
+    {
+
+    }
+
+    public void HandleWaterToggle(Toggle waterToggle)
+    {
+
+    }
+
+    public void HandleChargesToggle(Toggle chargesToggle)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
