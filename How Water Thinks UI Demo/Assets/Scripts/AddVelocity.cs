f4cf1006897705744aaf01143e37729c6da1356a
reesb <10052971+reesb@users.noreply.github.com>
2019-02-21 17:42:23 -0500

    2.21 Nick's changes at meeting that fixed atom speed problems

--- How Water Thinks UI Demo/Assets/Scripts/AddVelocity.cs
+++ How Water Thinks UI Demo/Assets/Scripts/AddVelocity.cs
@@ -9,7 +9,7 @@ public class AddVelocity : MonoBehaviour {
 	public float angularVelocityMultiplier = 0.0f;
 	Vector3 atomVelocity;
 	public float temperatureMultiplier = 0.5f;
-
+    public Vector3 forward;
 	// Use this for initialization
 	void Start () {
 
@@ -24,10 +24,17 @@ public class AddVelocity : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        /*
+        forward = gameObject.transform.forward;
 		atomVelocity = Vector3.forward * (Random.value - 0.05f) + Vector3.up * (Random.value - 0.05f) + Vector3.right * (Random.value - 0.05f);
 		rb.velocity += atomVelocity * velocityMultiplier * temperatureMultiplier;
-		rb.angularVelocity += atomVelocity * angularVelocityMultiplier;
-		
-	}
+		rb.angularVelocity += atomVelocity * angularVelocityMultiplier;*/
+
+        this.gameObject.GetComponent<Rigidbody>().drag = 0;
+        this.gameObject.GetComponent<Rigidbody>().angularDrag = 0;
+
+        int idealVelocity = 4;
+        //Debug.Log(this.gameObject.GetComponent<Rigidbody>().velocity);
+        this.gameObject.GetComponent<Rigidbody>().velocity = idealVelocity * this.gameObject.GetComponent<Rigidbody>().velocity.normalized;
+    }
 }
