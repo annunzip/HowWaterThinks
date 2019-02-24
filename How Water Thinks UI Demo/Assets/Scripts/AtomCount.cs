f20626eed18a7959c8003eddd967fad870cd287e
reesb <10052971+reesb@users.noreply.github.com>
2019-02-23 22:34:00 -0500

    2.23 added ability to count and display number of atoms outside and inside membrane, slightly modified equation from upper/lower to outer/inner

---
+++ How Water Thinks UI Demo/Assets/Scripts/AtomCount.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtomCount : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //int numAtoms = GameObject.FindGameObjectsWithTag("SodiumAtom").Length + GameObject.FindGameObjectsWithTag("ChlorineAtom").Length;
+        int numAtomsOuter = 0;
+        int numAtomsInner = 0;
+
+        foreach (GameObject atom in GameObject.FindGameObjectsWithTag("SodiumAtom")) {
+            if (atom.transform.position.y > 214.95) numAtomsOuter += 1;
+            else numAtomsInner += 1;
+        }
+        foreach (GameObject atom in GameObject.FindGameObjectsWithTag("ChlorineAtom"))
+        {
+            if (atom.transform.position.y > 214.95) numAtomsOuter += 1;
+            else numAtomsInner += 1;
+        }
+
+        GameObject.FindGameObjectWithTag("AtomsOutsideCounterText").GetComponent<Text>().text = "Atoms Outside: " + numAtomsOuter;
+        GameObject.FindGameObjectWithTag("AtomsInsideCounterText").GetComponent<Text>().text = "Atoms Inside: " + numAtomsInner;
+        //Debug.Log("numAtomsOuter = " + numAtomsOuter);
+        //Debug.Log("numAtomsInner = " + numAtomsInner);
+
+    }
+}
