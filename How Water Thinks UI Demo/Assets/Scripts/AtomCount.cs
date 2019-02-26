28618f770f945731398ce5e731896a6a702e9931
reesb <10052971+reesb@users.noreply.github.com>
2019-02-25 19:51:46 -0500

    2.25 added voltometer, but it's just text changing color atm

--- How Water Thinks UI Demo/Assets/Scripts/AtomCount.cs
+++ How Water Thinks UI Demo/Assets/Scripts/AtomCount.cs
@@ -12,23 +12,49 @@ public class AtomCount : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //int numAtoms = GameObject.FindGameObjectsWithTag("SodiumAtom").Length + GameObject.FindGameObjectsWithTag("ChlorineAtom").Length;
+        //Getting the number of atoms inside and outside
         int numAtomsOuter = 0;
         int numAtomsInner = 0;
+        float voltage = 0;
 
         foreach (GameObject atom in GameObject.FindGameObjectsWithTag("SodiumAtom")) {
-            if (atom.transform.position.y > 214.95) numAtomsOuter += 1;
-            else numAtomsInner += 1;
+            if (atom.transform.position.y > 214.95)
+            {
+                numAtomsOuter++;
+                voltage++;
+            }
+            else
+            {
+                numAtomsInner++;
+                voltage--;
+            }
         }
         foreach (GameObject atom in GameObject.FindGameObjectsWithTag("ChlorineAtom"))
         {
-            if (atom.transform.position.y > 214.95) numAtomsOuter += 1;
-            else numAtomsInner += 1;
+            if (atom.transform.position.y > 214.95)
+            {
+                numAtomsOuter++;
+                voltage--;
+            }
+            else
+            {
+                numAtomsInner++;
+                voltage++;
+            }
+
         }
         foreach (GameObject atom in GameObject.FindGameObjectsWithTag("PotassiumAtom"))
         {
-            if (atom.transform.position.y > 214.95) numAtomsOuter += 1;
-            else numAtomsInner += 1;
+            if (atom.transform.position.y > 214.95)
+            {
+                numAtomsOuter += 1;
+                voltage++;
+            }
+            else
+            {
+                numAtomsInner += 1;
+                voltage--;
+            }
         }
 
         GameObject.FindGameObjectWithTag("AtomsOutsideCounterText").GetComponent<Text>().text = "Atoms Outside: " + numAtomsOuter;
@@ -36,5 +62,13 @@ public class AtomCount : MonoBehaviour {
         //Debug.Log("numAtomsOuter = " + numAtomsOuter);
         //Debug.Log("numAtomsInner = " + numAtomsInner);
 
+        //Handline voltage
+        float maxVoltage = GameObject.FindGameObjectsWithTag("SodiumAtom").Length + GameObject.FindGameObjectsWithTag("ChlorineAtom").Length + GameObject.FindGameObjectsWithTag("PotassiumAtom").Length;
+
+
+        if (voltage > 0) GameObject.FindGameObjectWithTag("Voltage").GetComponent<Text>().color = new Color((voltage/maxVoltage*255f)/255f, 0f/255f, 0f/255f);
+        else if (voltage < 0) GameObject.FindGameObjectWithTag("Voltage").GetComponent<Text>().color = new Color(0f/0f, 0f/0f, (-voltage/maxVoltage*255f)/255f);
+        else GameObject.FindGameObjectWithTag("Voltage").GetComponent<Text>().color = new Color(0f/255f, 0f/255f, 0f/255f);
+        Debug.Log(voltage);
     }
 }
