bf64f90bb37f7f73e22dcf9ef03f0932c374a976
annunzip <patrick.annunz@gmail.com>
2019-04-17 15:54:05 -0400

    4-17 Added preset camera angles and adjusted menu stuff

--- How Water Thinks UI Demo/Assets/Scripts/Spherical_Camera.cs
+++ How Water Thinks UI Demo/Assets/Scripts/Spherical_Camera.cs
@@ -12,6 +12,9 @@ public class Spherical_Camera : MonoBehaviour
     private Vector3 point;
     private Quaternion startRotation;
     private Vector3 startPosition;
+    private ArrayList pos;
+    private ArrayList rot;
+    private int currentPos;
 
     // Use this for initialization
     void Start()
@@ -20,12 +23,64 @@ public class Spherical_Camera : MonoBehaviour
         transform.LookAt(point);
         startRotation = transform.rotation;
         startPosition = transform.position;
+        pos = new ArrayList();
+        rot = new ArrayList();
+        currentPos = 0;
+        addPresets();
+
+    }
+
+    public void addPresets()
+    {
+        //preset 1
+        pos.Add(new Vector3(660.53f,219.68f,-205.08f));
+        rot.Add(Quaternion.Euler(84.41f,79.30f,79.82f));
+        //preset 2
+        pos.Add(new Vector3(660.53f,210.01f,-205.04f));
+        rot.Add(Quaternion.Euler(-84.41f,85.94f,-85.35f));
+        //preset 3
+        pos.Add(new Vector3(658.91f,215.26f,-205.01f));
+        rot.Add(Quaternion.Euler(11.18f,89.56f,-40.94f));
+        //preset 4
+        //pos.Add(new Vector3(661.77f,215.00f,-204.66f));
+        //rot.Add(Quaternion.Euler(10.47f,-113.74f,118.33f));
+        //preset 5
+        pos.Add(new Vector3(660.85f,214.87f,-205.00f));
+        rot.Add(Quaternion.Euler(-6.22f,-89.77f,-178.30f));
+        //preset 6
+        pos.Add(new Vector3(660.35f,214.92f,-205.00f));
+        rot.Add(Quaternion.Euler(-6.22f,-89.77f,-178.304f));
+        //preset 7
+        pos.Add(new Vector3(663.63f,215.00f,-203.65f));
+        rot.Add(Quaternion.Euler(3.315f,-117.30f,94.40f));
+        
+        // Back to start (make sure this is always the last one added)
+        pos.Add(startPosition);
+        rot.Add(startRotation);
     }
 
     public void ResetCamera()
     {
         transform.rotation = startRotation;
         transform.position = startPosition;
+    }
+
+    public void PresetCamera()
+    {
+        //Debug.Log(currentPos);
+        if(currentPos != 0 && currentPos == (pos.Count))
+        {
+            transform.position = (Vector3)pos[0];
+            transform.rotation = (Quaternion)rot[0];
+            currentPos = 0;
+        }
+        else
+        {
+            transform.position = (Vector3)pos[currentPos];
+            transform.rotation = (Quaternion)rot[currentPos];
+            currentPos += 1;
+        }
+        
     }
 
     // Update is called once per frame
@@ -75,6 +130,11 @@ public class Spherical_Camera : MonoBehaviour
         if (Input.GetKey(KeyCode.R))
         {
             ResetCamera();
+            currentPos = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.Y))
+        {
+            PresetCamera();
         }
     }
 }
