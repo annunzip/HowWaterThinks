340ca316bcc090c69b1cfb98c56b7b0f6cf92ec2
annegchao <agchao22@gmail.com>
2019-04-18 23:49:07 -0400

    4.18 Added controls page to tutorial

--- How Water Thinks UI Demo/Assets/Scripts/Tutorial_Change.cs
+++ How Water Thinks UI Demo/Assets/Scripts/Tutorial_Change.cs
@@ -5,30 +5,47 @@ using UnityEngine.UI;
 using UnityEditor;
 
 public class Tutorial_Change : MonoBehaviour {
-    public Button backbtn;
     public Button nextbtn;
+    public Button backbtn;
+    public Button nextbtntwo;
+    public Button backbtntwo;
     public Canvas CanvasA;
-	public Canvas CanvasB;
+    public Canvas CanvasB;
+    public Canvas CanvasC;
 
-	// Use this for initialization
-	void Start () {
-        //Debug.Log(this.name);
-		Button btn1 = backbtn.GetComponent<Button>();
-		btn1.onClick.AddListener(TaskOnClick);
-        Button btn2 = nextbtn.GetComponent<Button>();
-        btn2.onClick.AddListener(TaskOnClick);
-        CanvasA.gameObject.SetActive(true);
+    // Use this for initialization
+    void Start () {
+	CanvasA.gameObject.SetActive(true);
         CanvasB.gameObject.SetActive(false);
+	CanvasC.gameObject.SetActive(false);
+	Button btn1 = nextbtn.GetComponent<Button>();
+	btn1.onClick.AddListener(CanvasAFunc);
+        Button btn2 = nextbtntwo.GetComponent<Button>();
+        btn2.onClick.AddListener(CanvasBNext);
+        Button btn3 = backbtn.GetComponent<Button>();
+        btn3.onClick.AddListener(CanvasBBack);
+        Button btn4 = backbtntwo.GetComponent<Button>();
+        btn4.onClick.AddListener(CanvasCBack);
     }
 	
 	// Update is called once per frame
-	void TaskOnClick() {
-		if(CanvasA.gameObject.activeSelf == true){
-			CanvasA.gameObject.SetActive(false);
-			CanvasB.gameObject.SetActive(true);
-		}else{
-			CanvasA.gameObject.SetActive(true);
-			CanvasB.gameObject.SetActive(false);
-		}
+	void CanvasAFunc() {
+		CanvasA.gameObject.SetActive(false);
+		CanvasB.gameObject.SetActive(true);
+	}
+	
+	void CanvasBNext(){
+		CanvasB.gameObject.SetActive(false);
+		CanvasC.gameObject.SetActive(true);
+	}
+
+	void CanvasBBack(){
+		CanvasB.gameObject.SetActive(false);
+		CanvasA.gameObject.SetActive(true);
+	}
+
+	void CanvasCBack(){
+		CanvasC.gameObject.SetActive(false);
+		CanvasB.gameObject.SetActive(true);
 	}
 }
